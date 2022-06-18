aa1608a6b5deac6d895bb55e063fd4d452b2befa
Logan <logan.scholz@gmail.com>
2022-06-18 11:32:17 -0700

    this commit, for sure

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 bool debug_startup = true; // set this false to stop the debug light sequence that
                            // turns all lights on one by one to make sure none are
                            // burnt out
-bool simple_light_pattern = true; // TODO - control this with an external switch
+bool simple_light_pattern = false; // TODO - control this with an external switch
 
-uint8_t light_sequence[6] = { 2, 3, A0, A1, A2, A3 };
+const int num_of_lights = 6;
+uint8_t light_sequence[num_of_lights] = { 2, 3, A0, A1, A2, A3 };
 
 int bright = 170;
 unsigned long rdm_delayer = 100;
@@ -14,13 +15,13 @@ unsigned long update_seed_time = 180000; // 180000 = 30 minutes (i think)
 unsigned long last_seed_update_time = 0;
 
 void allLightsOff() {
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     digitalWrite(light_sequence[i], LOW);
   }
 }
 
 void initLights() {
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     pinMode(light_sequence[i], OUTPUT);
   }
   allLightsOff();
@@ -28,14 +29,14 @@ void initLights() {
 }
 
 void testLights() {
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     digitalWrite(light_sequence[i], HIGH);
     delay(250);
     digitalWrite(light_sequence[i], LOW);
     delay(250);
   }
 
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     digitalWrite(light_sequence[i], HIGH);
     delay(250);
   }
@@ -44,7 +45,7 @@ void testLights() {
 }
 
 void randomOnOff() {
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     rdm_on_off = random(0,2);
     rdm_delayer = random(150, 600);
     digitalWrite(light_sequence[i], rdm_on_off);
@@ -57,14 +58,14 @@ void sequence1() {
   for(int i = 0; i <= rdm_num_repeats; i++) {
     int rdm_delay_time  = random(250, 750);
     allLightsOff();
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < num_of_lights; i++) {
       if(i%2==0) digitalWrite(light_sequence[i], HIGH);
       else digitalWrite(light_sequence[i], LOW);
     }
     delay(rdm_delay_time);
     rdm_delay_time  = random(250, 750);
     allLightsOff();
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < num_of_lights; i++) {
       if(i%2==0) digitalWrite(light_sequence[i], LOW);
       else digitalWrite(light_sequence[i], HIGH);
     }
@@ -74,12 +75,12 @@ void sequence1() {
 
 void sequence2() {
   allLightsOff();
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     digitalWrite(light_sequence[i], HIGH);
     delay(random(250-450));
   }
 
-  for(int i = 0; i < 6; i++) {
+  for(int i = 0; i < num_of_lights; i++) {
     digitalWrite(light_sequence[i], LOW);
     delay(random(250-450));
   }
